3b650ad3160dffc5b4a6b6999cea9e9e2e1365a5
shou-kato <nanades1@gmail.com>
2020-11-09 02:44:50 +0900

    first commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,65 +2,56 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'sho kato portfolio', // e.g: 'Name | Developer'
+  lang: 'ja', // e.g: en, es, fr, jp
+  description: 'sho kato portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Sho Kato Portfolio',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile.png',
+  paragraphOne: 'Sho Kato',
+  paragraphTwo: 'N高等学校2年生',
+  paragraphThree: '使用できるプログラミング言語 フレームワーク',
+  paragraphFour: 'html css nuxt.js react(勉強中)',
+  paragraphFive: '趣味',
+  paragraphSix: '寝ること',
+  paragraphSeven: '2020 09  〜 株式会社CYMES インターン',
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project.png',
+    title: 'オセロ',
+    info: 'オフラインで初めてオセロを作成しました。',
+    info2: 'siv3d',
+    url: 'https://github.com/shou-kato/siv3d_osero',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project.png',
+    title: 'オンラインオセロ',
+    info: 'オンラインで対戦できるオセロを作りました。',
+    info2: 'nuxt.js socket io',
+    url: 'https://github.com/shou-kato/socketIo_reverse',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'project.png',
+    title: '掲示板',
+    info: '昔から掲示板を作りたい思い作ってみました。',
+    info2: 'nuxt.js firebase(auth store)',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
 ];
-
-// CONTACT DATA
-export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
-};
 
 // FOOTER DATA
 export const footerData = {
@@ -72,18 +63,8 @@ export const footerData = {
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/shou-kato',
     },
   ],
 };
